c7717967274800b1b62e30a3c441445c9f5fbb86
Saketh <saketh@apples-macbook-pro.local>
2017-12-05 15:03:15 +0530

    Initial Commit

---
+++ Document Scanner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Document Scanner
+//
+//  Created by Saketh on 05/12/17.
+//  Copyright © 2017 Saketh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Document Scanner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Document Scanner
+//
+//  Created by Saketh on 05/12/17.
+//  Copyright © 2017 Saketh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
